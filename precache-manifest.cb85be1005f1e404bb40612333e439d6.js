7dc7b4e509d3d7d1b369cfa1647a184408fc722e
Rodrigo Lopes <rocostalopes@gmail.com>
2020-02-14 02:17:18 -0500

    Updates

---
+++ precache-manifest.cb85be1005f1e404bb40612333e439d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1db1348da5bda453da8d3103168ef42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "defcbea68b10cb07a6fa",
+    "url": "/static/js/2.aae56af8.chunk.js"
+  },
+  {
+    "revision": "9ce7c8cf7a9a7b89c7b075c364891379",
+    "url": "/static/js/2.aae56af8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c452af1436898e1a67f",
+    "url": "/static/js/main.125d4996.chunk.js"
+  },
+  {
+    "revision": "1314b0a305d26dc405c2",
+    "url": "/static/js/runtime-main.599e9d3e.js"
+  },
+  {
+    "revision": "bdc69844ad33f91a858f7971cc9292e9",
+    "url": "/static/media/regular.bdc69844.jpg"
+  },
+  {
+    "revision": "909d7a217f48dceb01f5d03f83a84315",
+    "url": "/static/media/thebigbangtheory.909d7a21.jpg"
+  }
+]);
